4a86df1731939adb5e07e5337d3b610fd907067a
denisgorkivenko81399-dotcom <denisgorkivenko81399@gmail.com>
2026-09-17 23:16:06 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -124,7 +124,7 @@ async function showMuseumDetails(museumId) {
     container.innerHTML = `
         <div style="text-align:center;padding:3rem 1rem;">
             <div class="loader-spinner" style="margin:0 auto 1rem;"></div>
-            <p style="color:#7b4a2e;font-weight:500;">Загрузка музея...</p>
+            <p style="color:#7b4a2e;font-weight:500;">Смотрительница обходит залы… Не шуметь!</p>
         </div>`;
     modal.querySelector('.close').onclick = () => modal.classList.add('hidden');
 
@@ -294,7 +294,7 @@ async function showExhibits(museumId) {
     container.innerHTML = `
         <div style="text-align:center;padding:2rem 1rem;">
             <div class="loader-spinner" style="margin:0 auto 1rem;"></div>
-            <p style="color:#7b4a2e;">Загрузка экспонатов...</p>
+            <p style="color:#7b4a2e;">Инвентаризация идёт… Экспонаты не выносить!</p>
         </div>`;
     modal.querySelector('.close').onclick = () => modal.classList.add('hidden');
 
